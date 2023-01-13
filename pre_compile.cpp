5eb920b62d62946afd6769213fe85658cc06165f
Moxie1337 <1779079267@qq.com>
2023-01-13 11:23:14 +0800

    test pre_compile

---
+++ pre_compile.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+#define N 10000
+#define ADD(a, b) ((a) + (b));
+
+int main()
+{
+    int c;
+
+    c = ADD(1, 2);
+
+    std::cout << N  << c << std::endl;
+
+    return 0;
+}
